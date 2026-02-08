f28db3503502a4f40110c1de9bb1cb79022f7dbb
Matt L. <matt.labriola@gmail.com>
2026-02-07 23:15:13 -0500

    Update my_wordle.py
    Started with functions

--- my_wordle.py
+++ my_wordle.py
@@ -1,27 +1,97 @@
 """ An attempt to create a 'wordle' style game"""
 import random
+import sys
 
-word_list = ['feel', 'dark', 'hear', 'cold', 'gone', 'list',
-             'fine', 'cool', 'card', 'cute', 'life', 'hope', 'calm', 
-             'away', 'say', 'hair', 'nine', 'exit', 'push', 'keep', 'fell', 
-             'fact', 'city', 'sell', 'rice', 'four', 'game', 'busy', 'able', 
-             'word', 'pull', 'back', 'five', 'baby', 'fair', 'base', 'hide', 'need', 'sale', 'chat']
-word = random.choice(word_list)
-print(word)
-# guess = 'thin'
-# if guess not in word_list:
- #   print("Not a word")
-#else:
-#    print("good word")
+WORD_LIST = [
+  'feel', 'dark', 'hear', 'cold', 'gone', 'list',
+  'fine', 'cool', 'card', 'cute', 'life', 'hope', 'calm', 
+  'away', 'say', 'hair', 'nine', 'exit', 'push', 'keep', 'fell', 
+  'fact', 'city', 'sell', 'rice', 'four', 'game', 'busy', 'able', 
+  'word', 'pull', 'back', 'five', 'baby', 'fair', 'base',
+  'hide', 'need', 'sale', 'chat'
+]
 
+MAX_GUESSES = 5 # Change Mac guessed to whatever practical number you need
+
+def get_secret_word() -> str:
+  """ Selects the secret word from a list"""
+  no params
+  returns: chosen word  
+  word = random.choice(WORD_LIST)
+  return word
+
+def intro():
+  """ Opening greeting and explanation """
+  pass
+
+def get_users_word() -> str:
+  """ Input routine 
+  called by loop.
+  no params
+  # guess = 'thin'
+  # if guess not in word_list:
+  #   print("Not a word!")
+  returns word (str)"""
+  pass
+
+def check_word(guess: str, secret_word: str) -> str:
+  """ Check routine
+  checks character by character for match and 
+  provides feedback to user.
+  param: guess: users input (str)
+  param: secret_word: randomly selected secret word. 
+  returns: feedback"""
+
+    if guess == secret_word:
+      return "You got it!"
+    clues = []
+
+    for i in range(len(guess)):
+        if guess[i] == secret_word[i]:
+            # A correct letter is in the correct place
+            clues.append('Fermi')
+        elif guess[i] in secret_word[i]:
+            # A correct letter is in the incorrect place
+            clues.append('Pico')
+    if len(clues) == 0:
+        return 'Bagels' # There are no correct digits at all
+    else:
+        # Sort the clues into alphabetical order so their order 
+        # doesn't give anything away
+        clues.sort()
+        # Make a single string from the list of clues
+        return ' '.join(clues)
+
+
+def main:
+  """ Main program """
+  intro()
+
+  secret_word = get_secret_word()
+
+  while True:
+    print("The word has been chosen. Begin.")
+    numGuesses = 1
+    while numGuesses <= MAX_GUESSES:
+        guess = ''
+        # keep looping until valid guess
+        while len(guess) != 4 or not guess.isstr():
+                print('Guess #{}: '.format(numGuesses))
+                guess = input('> ')
+                if guess == 'q':
+                    sys.exit()
+        for i in number_of_guesses:
+          guess = get_users_word()
+          check_word(guess, secret_word)
+
+     
+      
 # TODO
-# Use name - main
-# Use functions
-# Use proper
-# Write intro and rules
-# Use random.choice to choose a word
+
+
+
+# write intro and rules
 # Get user input
-# Check against word list
 # Check for character and positional matches
 # Give feedback
 # Loop input and checks
@@ -30,3 +100,5 @@ print(word)
 # Incorporate an exit strategy during play
 # Play again routine
 # Expand word list
+if __name__ == "__main__":
+  main()
